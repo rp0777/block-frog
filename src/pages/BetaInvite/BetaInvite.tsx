10b47ff1b05ea557408208b72b7c48c03c0a6abe
Rajat <rajatpatel.tcet@gmail.com>
2024-10-21 04:58:23 +0530

    fix/import errors in build

--- src/pages/BetaInvite/BetaInvite.tsx
+++ src/pages/BetaInvite/BetaInvite.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const BetaInvite = () => {
   return (
     <div>BetaInvite</div>
--- src/pages/Enquiry/Enquiry.tsx
+++ src/pages/Enquiry/Enquiry.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Enquiry = () => {
   return (
     <div>Enquiry</div>
